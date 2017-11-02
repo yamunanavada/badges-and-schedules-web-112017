5eac8c0cbf0bd6b4e9753e89e9ea6f97b499726a
Yamuna Navada <yamunanavada@gmail.com>
2017-11-02 20:23:51 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  badges = []
+  for i in array
+    badges.push("Hello, my name is #{i}.")
+  end
+  badges
+end
+
+
